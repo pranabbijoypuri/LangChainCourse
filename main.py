ca5be37d6a9a26e07ad53436f4c3f6381cd935a1
pranabbijoypuri <santubijoypuri@gmail.com>
2025-12-16 21:45:44 -0500

    Update the promt template file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from langchaincourse!")
+from dotenv import load_dotenv
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
 
-if __name__ == "__main__":
-    main()
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+print(OPENAI_API_KEY)
